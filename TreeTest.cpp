3acfc27de1ed9be21e5b0d1291f870fdc110b4fa
cyoni <44746539+cyoni@users.noreply.github.com>
2019-03-18 21:11:22 +0200

    Add files via upload

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -17,16 +17,32 @@ int main() {
   threetree.insert(7);
   threetree.insert(3);
   
+  ariel::Tree mytree;  
+
+  mytree.insert(15);
+  mytree.insert(20);
+  mytree.insert(10);
+  mytree.insert(5);
+  mytree.insert(8);
+  mytree.insert(25);
+  
   badkan::TestCase tc("Binary tree");
   tc
+
+  .CHECK_EQUAL (mytree.size(), 6)
+  .CHECK_THROWS    (mytree.remove(100))
+  .CHECK_EQUAL (mytree.left(10) ,5)
+  .CHECK_EQUAL (mytree.parent(8) , 10)
+
   .CHECK_EQUAL (emptytree.size(), 0)
   .CHECK_OK    (emptytree.insert(5))
   .CHECK_EQUAL (emptytree.size(), 1)
   .CHECK_EQUAL (emptytree.contains(5), true)
   .CHECK_OK    (emptytree.remove(5))
+  .CHECK_EQUAL (emptytree.contains(5), false)
   .CHECK_THROWS(emptytree.remove(5))
   .CHECK_EQUAL (emptytree.size() ,0)
-  
+
   .CHECK_EQUAL (threetree.size(), 3)
   .CHECK_EQUAL (threetree.root(), 5)
   .CHECK_EQUAL (threetree.parent(3), 5)
@@ -34,8 +50,8 @@ int main() {
   .CHECK_EQUAL (threetree.left(5), 3)
   .CHECK_EQUAL (threetree.right(5), 7)
   .CHECK_THROWS(threetree.insert(3))
+  .CHECK_THROWS(threetree.left(6))
   .CHECK_OK    (threetree.print())
-  
   .print();
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
