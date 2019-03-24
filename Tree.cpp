abe0e2d2c5f8121d3443b9fc4b96bc001f8a4a67
cyoni <44746539+cyoni@users.noreply.github.com>
2019-03-24 19:56:47 +0200

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -20,10 +20,93 @@ int ariel::Tree::size()
 
 void ariel::Tree::remove(int number)
 {
+
 	if (sizeofTree==0){
-	throw std::invalid_argument("Tree is empty");	
+
+		throw std::invalid_argument("Tree is empty");
+		
 	}
+	int isLeft=0;
+	int found = contains(number);
+
+	if (found==1){
+	if (size()==1 && number == root()){
+	delete head;
+	head=NULL;
+	sizeofTree--;
+	return;
+	}
+
+        Node *current = head;
+	Node *parent=head;
+
+	while (current->data!=number){
+	parent=current;
+
+	if (current->data > number) {current=current->left;isLeft=0;}
+	else {current=current->right; isLeft=1;}
+	}
+
+	// 1. leaf
+
+	if (current->left == NULL  && current->right == NULL){
+
+	delete current;
+	if (isLeft == 0) parent->left=NULL;
+	else parent->right=NULL;
+
+	}
+	// 3. the deleted node has two children
+	else if (current->left != NULL && current->right != NULL) {
+///////////////////////////////////
+
+	Node *succ = current;
+	Node *succparent = current;
+	Node *del=current;
+
+	current = current->right;
+	while (current != NULL){
+	succparent=succ;
+	succ=current;
+	current=current->left;
 	
+	}
+	if (succ!=del->right){
+	succparent->left=succ->right;
+	succ->right=del->right;
+	}
+
+	current=del;
+	if (current==head) head=succ;
+	else if(isLeft==0) parent->left=succ;
+	else parent->right=succ;
+	succ->left=current->left;
+	
+
+//////////////////////////////////
+	}
+	else{ // 2. it has only one child
+	
+	if (current->right == NULL){
+	if (current == head) head= current->left;
+	else {if (isLeft==0) parent->left=current->left;
+	else parent->right=current->left;}}
+	
+	else{ if (current->left == NULL){
+	if (current == head) {head= current->right;}
+	else {if (isLeft==0) parent->left=current->right;
+	else {parent->right=current->right;}	}
+	} }
+
+	
+	}
+
+	sizeofTree--;
+	} 
+	else{
+	throw std::invalid_argument("The number you are trying to remove does not exist");
+	}
+
 	
 }
 
@@ -55,91 +138,20 @@ return head->data;
 int ariel::Tree::parent(int i)
 {
 
-   int found = contains(i);
-
-    if (found == 0 || head->data == i){
-       throw std::invalid_argument("check your input & tree");
-    } 
-
-
-  	Node *current = head;
-	Node *parent=NULL;
-	
-	while (current != NULL){
-
-	
-	if (current->data == i){
-	break;
-	}
-
-	parent=current;
-	if (current->data > i) current=current->left;
-	else current=current->right;
-	}
-	
-    return parent->data;
+    return NULL;
 }
 
 
 int ariel::Tree::left(int i)
 {
-  int found = contains(i);
-
-    if (found == 0){
-   throw std::invalid_argument("The number does not exist");
-	return 0;
-    }
-
-  	Node *current = head;
-	Node *parent=NULL;
-	
-	while (current != NULL){
-
-	
-	if (current->data == i){
-	break;
-	}
-
-	parent=current;
-	if (current->data > i) current=current->left;
-	else current=current->right;
-	}
-	
-	if (current->left == NULL){
-       throw std::invalid_argument("no left son");
-	
-	}
-    return current->left->data;
+  
+    return NULL;
 }
 
 int ariel::Tree::right(int i)
 {
- int found = contains(i);
-
-    if (found == 0){
-       throw std::invalid_argument("The number does not exist");
-    }
-
-  	Node *current = head;
-	Node *parent=NULL;
-	
-	while (current != NULL){
-
-	
-	if (current->data == i){
-	break;
-	}
-
-	parent=current;
-	if (current->data > i) current=current->left;
-	else current=current->right;
-	}
-	
-	if (current->right == NULL){
-       throw std::invalid_argument("No right son");
-
-	}
-    return current->right->data;
+ 
+    return NULL;
 }
 
 void ariel::Tree::print()
