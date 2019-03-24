7a8219157c3ff3cc0e28fef3a7078e6350feeb73
cyoni <44746539+cyoni@users.noreply.github.com>
2019-03-24 19:54:42 +0200

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -20,93 +20,10 @@ int ariel::Tree::size()
 
 void ariel::Tree::remove(int number)
 {
-
 	if (sizeofTree==0){
-
-		throw std::invalid_argument("Tree is empty");
-		
+	throw std::invalid_argument("Tree is empty");	
 	}
-	int isLeft=0;
-	int found = contains(number);
-
-	if (found==1){
-	if (size()==1 && number == root()){
-	delete head;
-	head=NULL;
-	sizeofTree--;
-	return;
-	}
-
-        Node *current = head;
-	Node *parent=head;
-
-	while (current->data!=number){
-	parent=current;
-
-	if (current->data > number) {current=current->left;isLeft=0;}
-	else {current=current->right; isLeft=1;}
-	}
-
-	// 1. leaf
-
-	if (current->left == NULL  && current->right == NULL){
-
-	delete current;
-	if (isLeft == 0) parent->left=NULL;
-	else parent->right=NULL;
-
-	}
-	// 3. the deleted node has two children
-	else if (current->left != NULL && current->right != NULL) {
-///////////////////////////////////
-
-	Node *succ = current;
-	Node *succparent = current;
-	Node *del=current;
-
-	current = current->right;
-	while (current != NULL){
-	succparent=succ;
-	succ=current;
-	current=current->left;
 	
-	}
-	if (succ!=del->right){
-	succparent->left=succ->right;
-	succ->right=del->right;
-	}
-
-	current=del;
-	if (current==head) head=succ;
-	else if(isLeft==0) parent->left=succ;
-	else parent->right=succ;
-	succ->left=current->left;
-	
-
-//////////////////////////////////
-	}
-	else{ // 2. it has only one child
-	
-	if (current->right == NULL){
-	if (current == head) head= current->left;
-	else {if (isLeft==0) parent->left=current->left;
-	else parent->right=current->left;}}
-	
-	else{ if (current->left == NULL){
-	if (current == head) {head= current->right;}
-	else {if (isLeft==0) parent->left=current->right;
-	else {parent->right=current->right;}	}
-	} }
-
-	
-	}
-
-	sizeofTree--;
-	} 
-	else{
-	throw std::invalid_argument("The number you are trying to remove does not exist");
-	}
-
 	
 }
 
